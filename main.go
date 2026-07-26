1410c172816c6b5c116ece3e92c29dcebfce5128
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2026-07-26 15:09:39 +0500

    feat: send EHLO and handle multilne SMTP responses

--- main.go
+++ main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 )
 
 func main() {
-
 	conn, err := net.Dial("tcp", "smtp.gmail.com:587")
-
 	if err != nil {
 		panic(err)
 	}
-
 	defer conn.Close()
 
-	buffer := make([]byte, 1024)
+	reader := bufio.NewReader(conn)
 
-	n, err := conn.Read(buffer)
+	// Read server greeting
+	greeting, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(greeting)
 
+	// Send EHLO
+	_, err = fmt.Fprintf(conn, "EHLO localhost\r\n")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(buffer[:n]))
+	// Read all EHLO response lines
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
 
+		fmt.Print(line)
+
+		// Last line starts with "250 "
+		if len(line) >= 4 && line[:4] == "250 " {
+			break
+		}
+	}
 }
