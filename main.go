bf7951a510175a9110f24271030ff2ec49c12076
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2026-07-27 11:49:56 +0500

    feat: send emails using the SMTP protocol

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func main() {
 	fmt.Fprintf(conn, "EHLO localhost\r\n")
 	lines, _ := readSMTPResponse(reader)
 
+	// Verify STARTTLS support
 	startTLS := false
 	for _, line := range lines {
 		if strings.Contains(line, "STARTTLS") {
@@ -69,6 +70,7 @@ func main() {
 	fmt.Fprintf(conn, "STARTTLS\r\n")
 	readSMTPResponse(reader)
 
+	// Upgrade existing connection
 	tlsConn := tls.Client(conn, &tls.Config{
 		ServerName: "smtp.gmail.com",
 	})
@@ -83,20 +85,55 @@ func main() {
 	readSMTPResponse(reader)
 
 	// AUTH LOGIN
-	fmt.Println("\n>>> AUTH LOGIN")
-
 	fmt.Fprintf(tlsConn, "AUTH LOGIN\r\n")
 	readSMTPResponse(reader)
 
-	// Username
 	username := base64.StdEncoding.EncodeToString([]byte(email))
 	fmt.Fprintf(tlsConn, "%s\r\n", username)
 	readSMTPResponse(reader)
 
-	// Password
 	pass := base64.StdEncoding.EncodeToString([]byte(password))
 	fmt.Fprintf(tlsConn, "%s\r\n", pass)
 	readSMTPResponse(reader)
 
-	fmt.Println("\nAuthentication Complete")
+	// MAIL FROM
+	fmt.Println("\n>>> MAIL FROM")
+	fmt.Fprintf(tlsConn, "MAIL FROM:<%s>\r\n", email)
+	readSMTPResponse(reader)
+
+	// RCPT TO
+	recipient := os.Getenv("SMTP_RCPT") // Change if you want
+
+	fmt.Println("\n>>> RCPT TO")
+	fmt.Fprintf(tlsConn, "RCPT TO:<%s>\r\n", recipient)
+	readSMTPResponse(reader)
+
+	// DATA
+	fmt.Println("\n>>> DATA")
+	fmt.Fprintf(tlsConn, "DATA\r\n")
+	readSMTPResponse(reader)
+
+	// Message
+	message := `From: ` + email + `
+To: ` + recipient + `
+Subject: Tiny SMTP Client
+
+Assalamu Alaikum!
+
+This email was sent manually using my own SMTP client written in Go.
+
+Regards,
+Ahmad
+`
+
+	// SMTP requires CRLF line endings
+	message = strings.ReplaceAll(message, "\n", "\r\n")
+
+	fmt.Fprintf(tlsConn, "%s\r\n.\r\n", message)
+	readSMTPResponse(reader)
+
+	// QUIT
+	fmt.Println("\n>>> QUIT")
+	fmt.Fprintf(tlsConn, "QUIT\r\n")
+	readSMTPResponse(reader)
 }
